ab092b8b95fc366158f475d386ff2fafb27f4e4f
Fabio Lima <fabiolimma125@gmail.com>
2023-05-08 21:06:57 -0300

    ainda no inicio

--- Client/client.go
+++ Client/client.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
 func main() {
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*300)
+
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, "GET", "http://localhost/8080", nil)
+	if err != nil {
+		log.Println("Erro ao tentar acessar o server", err)
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Println("Erro ao retornar a resposta do Server", err)
+	}
+
+	// defer resp.Body.Close()
+	// io.Copy(os.Stdout, resp.Body)
+	fmt.Println(resp)
 
 }
--- Server/server.go
+++ Server/server.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
 func main() {
+	fmt.Println("iniciando server....")
+	http.HandleFunc("/", ApiCotacao)
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func ApiCotacao(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("iniciando Api....")
+	req, err := http.Get("https://economia.awesomeapi.com.br/json/last/USD-BRL")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Erro na requisição da url %v \n", err)
+	}
+	resp, err := io.ReadAll(req.Body)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Erro na resposta da url %v \n", err)
+	}
+
+	fmt.Println("chegou aqui")
+
+	fmt.Println(string(resp))
 
 }
